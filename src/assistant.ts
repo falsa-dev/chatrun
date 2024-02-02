30c850f9d3d794b63e92b0169c05a501da2692a9
Sarim Abbas <3000809+sarimabbas@users.noreply.github.com>
2024-02-01 21:27:48 -0800

    wip

--- src/assistant.ts
+++ src/assistant.ts
@@ -31,4 +31,65 @@ export const createAssistant = () =>
     ],
   });
 
-const;
+/**
+ * @param chat - natural language describing what to do with the CLI
+ * @param run - the CLI to run
+ * @returns the final CLI command that will help accomplish the natural language task
+ */
+export const chatRun = async (chat: string, run: string): Promise<string> => {
+  // create thread
+  const thread = await openai.beta.threads.create({
+    messages: [
+      {
+        role: "user",
+        content: chat,
+      },
+    ],
+  });
+
+  // create assistant
+  const assistant = await createAssistant();
+
+  // run thread with assistant
+  let threadRun = await openai.beta.threads.runs.create(thread.id, {
+    assistant_id: assistant.id,
+  });
+
+  const time = Date.now();
+  // not more than 5 minutes
+  while (true && Date.now() - time < 1000 * 60 * 5) {
+    // retrieve thread
+    threadRun = await openai.beta.threads.runs.retrieve(
+      thread.id,
+      threadRun.id
+    );
+
+    // if run is errored, return an empty string
+    if (
+      threadRun.status === "expired" ||
+      threadRun.status === "failed" ||
+      threadRun.status === "cancelled" ||
+      threadRun.status === "cancelling"
+    ) {
+      console.log("🛑");
+      return "";
+    }
+
+    // if run is pending, wait and try again
+    if (threadRun.status === "queued" || threadRun.status === "in_progress") {
+      console.log("⌛");
+      sleep(1500);
+      continue;
+    }
+
+    // if a tool is required, use it
+    if (threadRun.status === "requires_action") {
+    }
+  }
+};
+
+/**
+ * @param ms - time in milliseconds
+ * @returns a promise that resolves after the given time
+ */
+export const sleep = (ms: number) => new Promise((r) => setTimeout(r, ms));
